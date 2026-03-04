af5dd21ec36eaacae3411d69b3126258f7844df8
unknown <charlie.fay21@gmail.com>
2026-03-03 23:49:17 -0300

    Ajuste de comentários

--- automacaocoleta.py
+++ automacaocoleta.py
@@ -2,27 +2,28 @@ import pyautogui
 import time
 import random
 
-# Configurações de randomização do tempo de espera
+#Configuração de humanização para randomizar tempo de espera
+#Nota: Randomização fixa, posteriormente, deve-se realizar essa randomização diretamente no código
 tempo_colheita= random.uniform(1.3, 1.6)
 tempo_caminhada = random.uniform(2.5, 3)
 
 
-# --- CONFIGURAÇÕES E SEGURANÇA ---
+#Configuração global de segurança
 pyautogui.PAUSE = 0.5
 pyautogui.FAILSAFE = True
 
-# --- COORDENADAS FIXAS (Ajuste conforme sua tela) ---
-
-# Os 9 slots de colheita (relativos à posição central do boneco)
+# Os 9 slots de colheita
 SPOT_COLETA = [
     (925, 512), (908, 725), (812, 500),
     (1078, 507), (1197, 425), (986, 345),
     (862, 296), (773, 472), (650, 577)
 ]
 
+#Otimizar esse bloco posteriormente para poupar linhas e melhorar a eficiência
+#Bloco apenas para teste
 POSICAO_MONTARIA = [
-    [(1225, 620), (866,416)], #posição terreno 1
-    [(1225, 620), (866,416) ], #posição terreno 2
+    [(1225, 620), (866,416)], #1
+    [(1225, 620), (866,416) ], #2
     [(1225, 620), (866,416) ], #3
     [(1225, 620), (866,416) ], #4
     [(1225, 620), (866,416) ], #5 
@@ -46,108 +47,108 @@ SPOT_PLANTACAO = [
     (1340, 514), (1216, 605), (1082, 701)
 ]
 
-# MAPA DE NAVEGAÇÃO (Aqui está a mágica dos 16 terrenos)
+# MAPA DE NAVEGAÇÃO
 # Cada sub-lista [ ] contém os cliques necessários para chegar e CENTRALIZAR no terreno
 CAMINHOS_DOS_16_TERRENOS = [
-    [(1135, 158), (798, 317)],                # Cliques para chegar no Terreno 1
-    [(1483, 172), (744, 308)],#, (747, 302)],   # Cliques para sair do 1 e chegar no 2
-    [(1522, 358), (1142, 377), (793, 323)],#, (747, 302)],   # Cliques para sair do 2 e chegar no 3
-    [(75, 427), (798, 271), (764, 297)],
-    [(725, 1079), (725, 1079), (964, 580), (1168, 301), (802, 329)], #terreno 5
-    [(1584, 478), (1773, 1079), (1082, 1079), (1471, 1079), (1072, 691), (1123, 321), (793, 323)], #terreno 6
-    [(1258, 313), (1163, 364), (793, 323)], #terreno 7
-    [(1332, 1079), (1072, 912), (1164, 335), (793, 323)], #terreno 8
-    [(1384, 531), (1134, 345), (793, 323)], #terreno 9
-    [(1280, 1079), (1130, 895), (1153, 336), (793, 323)], #terreno 10
+    [(1135, 158), (798, 317)],# Coordenadas para chegar da posição inicial até o terreno 1
+    [(1483, 172), (744, 308)],#, 1 > 2
+    [(1522, 358), (1142, 377), (793, 323)],# 2 > 3
+    [(75, 427), (798, 271), (764, 297)], # 3 > 4
+    [(725, 1079), (725, 1079), (964, 580), (1168, 301), (802, 329)], # 4 > 5
+    [(1584, 478), (1773, 1079), (1082, 1079), (1471, 1079), (1072, 691), (1123, 321), (793, 323)], # 5 > 6
+    [(1258, 313), (1163, 364), (793, 323)], # 6 > 7
+    [(1332, 1079), (1072, 912), (1164, 335), (793, 323)], # 7 > 8
+    [(1384, 531), (1134, 345), (793, 323)], # 8 > 9
+    [(1280, 1079), (1130, 895), (1153, 336), (793, 323)], # 9 > 10
     
-    [(637, 1079), (637, 1079), (525, 952), (1181, 341), (793, 323)], #terreno 11 fazer manutenção no terreno 11
+    [(637, 1079), (637, 1079), (525, 952), (1181, 341), (793, 323)], # 10 > 11 (necessita manutenção)
     
-    [(157, 1079), (639, 1068), (1191, 359), (793, 323)], #terreno 12
-    [(2, 782), (811, 565), (1152, 334), (793, 323)], #terreno 13
-    [(1919, 416), (1919, 1001), (781, 1079), (899, 1079), (932, 571), (1199, 296), (793, 323)], #terreno 14
-    [(1812, 565), (1152, 338), (793, 323)], #terreno 15
-    [(851, 1079), (851, 1079), (703, 1050), (1161, 326), (793, 323)], #terreno 16
+    [(157, 1079), (639, 1068), (1191, 359), (793, 323)], # 11 > 12
+    [(2, 782), (811, 565), (1152, 334), (793, 323)], # 12 > 13
+    [(1919, 416), (1919, 1001), (781, 1079), (899, 1079), (932, 571), (1199, 296), (793, 323)], # 13 > 14
+    [(1812, 565), (1152, 338), (793, 323)], # 14 > 15
+    [(851, 1079), (851, 1079), (703, 1050), (1161, 326), (793, 323)], # 15 > 16
 ]
 
-# --- FUNÇÕES DE AÇÃO ---
 
+#Funções de ações
+#Futuramente ajustar essa parte para deixar o código mais organizado e limpo
+
+#Função de colher as plantações
 def realizar_colheita():
     print(f"Iniciando Colheita e Montaria no Terreno...")
     
     # 1. Primeiro ele colhe os 9 slots
-    # Se não precisar segurar nada, apague a linha do 'with' e alinhe o 'for' à esquerda
     with pyautogui.hold('shift'): 
         for x, y in SPOT_COLETA:
             pyautogui.click(x, y, duration=1)
             time.sleep(tempo_colheita)
     
-    
-
+#Função de pegar semente
 def preparar_semente():
     time.sleep(3)
     print(f"Pegando semente no inventário...")
     pyautogui.press("i")
-    pyautogui.click(1676, 540, duration=0.5) # Slot da semente
-    pyautogui.click(822, 427, duration=0.5)  # Seleciona
+    pyautogui.click(1676, 540, duration=0.5)
+    pyautogui.click(822, 427, duration=0.5)
 
+#Função de plantar as sementes nos 9 slots de cada terreno
 def realizar_plantacao(indice_terreno):
     print(f"Plantando no terreno {indice_terreno + 1}...")
-    # 1. Faz a plantação normal
+    
+    #------Comentado para manutenção------
     #for x, y in SPOT_PLANTACAO:
         #pyautogui.click(x, y, duration=0.5)
         #time.sleep(random.uniform(1.1, 1.4))
 
-    # --- AJUSTE DA MONTARIA ---
-    # Busca a sublista de cliques para o terreno atual
+    #Ajusta o boneco para pegar a montaria e navegar nos terrenos
     time.sleep(3)
     passos_montaria = POSICAO_MONTARIA[indice_terreno]
     
-    print(f"Executando {len(passos_montaria)} cliques para estabilizar montaria...")
+    print(f"Executando {len(passos_montaria)} para subir na montaria...")
     
     for x, y in passos_montaria:
-        # Usa botão direito para garantir que o boneco apenas ande
         pyautogui.rightClick(x, y, duration=0.8)
-        # Espera o boneco chegar no ponto antes do próximo clique ou de montar
         time.sleep(random.uniform(2.5, 2.8)) 
 
-    # Agora que ele percorreu todos os passos, ele monta
+    #Sobe na montaria
     pyautogui.press("a")
     print("Montagem estabilizada!")
     time.sleep(random.uniform(0.7, 1.2))
     pyautogui.press("esc")
 
+#Função de caminhar entre os terrenos de acordo com as coordenadas passadas nas listas e sublistas
 def navegar_para_terreno(lista_de_cliques):
-    """Executa a sequência de cliques para o boneco andar até o próximo centro"""
     for x, y in lista_de_cliques:
         pyautogui.rightClick(x, y, duration=0.9)
         time.sleep(tempo_caminhada)
     pyautogui.press("a")
     
 
-# --- LOOP PRINCIPAL (A EXECUÇÃO) ---
-
+#Função Principal Inicialização
 def iniciar_bot():
     print("O bot começará em 3 segundos. Mude para a janela do jogo!")
     time.sleep(3)
 
-    # O loop 'for' percorre cada terreno da nossa lista de 16
+    # O loop percorre cada terreno da lista de 16
     for indice, cliques_trajeto in enumerate(CAMINHOS_DOS_16_TERRENOS):
         num_terreno = indice + 1
-        print(f"--- INICIANDO TERRENO {num_terreno} ---")
+        print(f"INICIANDO TERRENO {num_terreno}")
 
-        # 1. Movimentação (O boneco segue os pontos de clique até o centro)
+        #Chama função de navegar nos terrenos
         navegar_para_terreno(cliques_trajeto)
         
-        # 2. Ações no local (Agora que ele parou no centro)
+        #Chama as funções de ações no terreno
 
+        #comentado para manutenção
         #realizar_colheita()
         #preparar_semente()
         realizar_plantacao(indice)
 
-        print(f"--- TERRENO {num_terreno} FINALIZADO ---\n")
+        print(f"TERRENO {num_terreno} FINALIZADO\n")
 
     print("Ciclo de 16 terrenos completo!")
 
-# Executa o bot
+#Executa o bot
 if __name__ == "__main__":
     iniciar_bot()
--- cordX_Y.py
+++ cordX_Y.py
@@ -3,13 +3,13 @@ import time
 
 try:
     while True:
-        # Captura a posição atual
+        #Captura a posição do mouse
         x, y = pyautogui.position()
         
-        # Formata a saída para apagar a linha anterior e mostrar a nova
+        #Formata a saída para apagar a entrada anterior
         posicao = f"X: {str(x).rjust(4)} Y: {str(y).rjust(4)}"
         print(posicao, end="\r", flush=True)
         
-        time.sleep(0.1) # Pequeno intervalo para não sobrecarregar o processador
+        time.sleep(0.1) #Intervalo de atualização entre uma coordenada e outra
 except KeyboardInterrupt:
     print("\nLocalização finalizada.")
