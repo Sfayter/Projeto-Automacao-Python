4b5ad50dd154f4d6577dd8e9f45c02ae1e0f2795
unknown <charlie.fay21@gmail.com>
2026-04-30 10:02:37 -0300

    feat: coordenadas fazenda 3

--- main.py
+++ main.py
@@ -10,44 +10,48 @@ def iniciarAcoes():
     time.sleep(3)
 
     #Loop para percorrer nos terrenos
-    for i in range(2):
-        for indice, cliques_trajeto in enumerate(caminhosTerrenos):
-            numTerreno = indice + 1
-            print(f"Iniciando processo/terreno {numTerreno}")
+    for ilha in range(3):
+        if ilha < 2:
+            for indice, cliques_trajeto in enumerate(caminhosTerrenos):
+                numTerreno = indice + 1
+                print(f"Iniciando processo/terreno {numTerreno}")
 
-            #Chama função de navegar nos terrenos
-            navegarTerreno(cliques_trajeto)
+                #Chama função de navegar nos terrenos
+                navegarTerreno(cliques_trajeto)
 
-            #Funções de ação
-            realizarColheita()
+                #Funções de ação
+                realizarColheita()
 
-            #Estrutura de decisão necessária para fazer a troca de semente de acordo com o número do terreno na fazenda 2
-            if 12 <= numTerreno <= 16 and i == 1:
-                sementeHorta(numTerreno)
-            else:
-                prepararSemente()
+                #Estrutura de decisão necessária para fazer a troca de semente de acordo com o número do terreno na fazenda 2
+                if 12 <= numTerreno <= 16 and ilha == 1:
+                    sementeHorta(numTerreno)
+                else:
+                    prepararSemente()
 
-            realizarPlantacao(indice)
-            print(f"Terreno {numTerreno} finalizado.\n")
+                realizarPlantacao(indice)
+                print(f"Terreno {numTerreno} finalizado.\n")
+                
+            print("Ciclo de 16 terrenos completo!")
+
+            #Loop para guardar itens coletados e viajar entre as ilhas
+            if ilha <= 1:
+                for indice, cliques_trajeto in enumerate(caminhosIlhas):
+                    quantidade = indice + 1
+                    navegarIlha(cliques_trajeto, descerMontaria=5 if indice == 0 else None)
+                    if quantidade == 1:
+                        guardar_itens()
+                    elif quantidade == 2:
+                        print("Viajando entre ilhas...")
+                        time.sleep(3)
+                        viajar_ilha(ilhas[ilha])
+                        time.sleep(12)
+                #Funções para pegar itens e posicionar o personagem na ilha 2 para realizar o processo de colheita e plantação.
+                time.sleep(1.5)
+                pegar_itens()
+                posicionarIlha()
+        else:
+            print("Iniciando processo na ilha 3...")
             
-        print("Ciclo de 16 terrenos completo!")
-
-        #Loop para guardar itens coletados e viajar entre as ilhas
-        if i <= 1:
-            for indice, cliques_trajeto in enumerate(caminhosIlhas):
-                quantidade = indice + 1
-                navegarIlha(cliques_trajeto, descerMontaria=5 if indice == 0 else None)
-                if quantidade == 1:
-                    guardar_itens()
-                elif quantidade == 2:
-                    print("Viajando entre ilhas...")
-                    time.sleep(3)
-                    viajar_ilha(ilhas[i])
-                    time.sleep(12)
-            #Funções para pegar itens e posicionar o personagem na ilha 2 para realizar o processo de colheita e plantação.
-            time.sleep(1.5)
-            pegar_itens()
-            posicionarIlha()
     
 #Executa o bot
 if __name__ == "__main__":
